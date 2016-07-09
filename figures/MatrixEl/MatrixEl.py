b61fb28d2c5bfe0e90adac562e5feb198d0b86e7
Sebastian Liebschner <sebastian-liebschner@gmx.de>
2016-07-09 12:53:13 +0200

    changes, presumably mainly in Virt_Real chapter

--- figures/MatrixEl/MatrixEl.py
+++ figures/MatrixEl/MatrixEl.py
@@ -72,28 +72,28 @@ plt.rcParams.update({'font.size': 22})
 plt.xlabel(r"$t$ in $10^6$ GeV$^2$")#, size = 22)
 
 # plot without poles
-#plt.ylabel("(Absolute) Squared Matrix Element")
-#plt.axis([min(t/10**6) - 1.1,max(t/10**6) + 1.1,0,1.4])
-#plt.plot(t/10**6, matrix_LO_Tree, lw=2, ls="-", c='black', label = r"$\frac{1}{4\cdot 9}\sum|\mathcal{M}^{\mathrm{B}}_{\mathrm{MRSSM}}|^2$ mit LO $\alpha_s$")
-#plt.plot(t/10**6, matrix_NLO_Tree, lw=2, ls="--", c='red', label = r"$\frac{1}{4\cdot 9}\sum|\mathcal{M}^{\mathrm{B}}_{\mathrm{MRSSM}}|^2$ mit NLO $\alpha_s$")
-#plt.plot(t/10**6, matrix_NLO_Tree+matrix_NLO_Virt, lw=2, ls="-", c='red', label = r"$\frac{1}{4\cdot 9}\sum|\mathcal{M}^{\mathrm{B}}_{\mathrm{MRSSM}}|^2 + \frac{1}{4\cdot 9}\sum(\mathcal{M}^{\mathrm{V}}_{\mathrm{MRSSM}})^2$ mit NLO $\alpha_s$")
-#first_window.fill_between(t/10**6, matrix_NLO_Tree, matrix_NLO_Tree+matrix_NLO_Virt, 
-#facecolor=[1,90./100,90./100], color =[1,80./100,80./100], linestyle="-", label = r"$\frac{1}{4\cdot 9}\sum(\mathcal{M}^{\mathrm{V}}_{\mathrm{MRSSM}})^2$ mit NLO $\alpha_s$")
+plt.ylabel("(Absolute) Squared Matrix Element")
+plt.axis([min(t/10**6) - 1.1,max(t/10**6) + 1.1,0,1.4])
+plt.plot(t/10**6, matrix_LO_Tree, lw=2, ls="-", c='black', label = r"$\frac{1}{4\cdot 9}\sum|\mathcal{M}^{\mathrm{B}}_{\mathrm{MRSSM}}|^2$ mit LO $\alpha_s$")
+plt.plot(t/10**6, matrix_NLO_Tree, lw=2, ls="--", c='red', label = r"$\frac{1}{4\cdot 9}\sum|\mathcal{M}^{\mathrm{B}}_{\mathrm{MRSSM}}|^2$ mit NLO $\alpha_s$")
+plt.plot(t/10**6, matrix_NLO_Tree+matrix_NLO_Virt, lw=2, ls="-", c='red', label = r"$\frac{1}{4\cdot 9}\sum|\mathcal{M}^{\mathrm{B}}_{\mathrm{MRSSM}}|^2 + \frac{1}{4\cdot 9}\sum 2 \Re \left( \mathcal{M}^{\mathrm{B}} \mathcal{M}^{\mathrm{1L}\ast} \right)$ mit NLO $\alpha_s$")
+first_window.fill_between(t/10**6, matrix_NLO_Tree, matrix_NLO_Tree+matrix_NLO_Virt, 
+facecolor=[1,90./100,90./100], color =[1,80./100,80./100], linestyle="-", label = r"$\frac{1}{4\cdot 9}\sum 2 \Re \left( \mathcal{M}^{\mathrm{B}} \mathcal{M}^{\mathrm{1L}\ast} \right)$ mit NLO $\alpha_s$")
 
 # plot with poles
-#plt.ylabel(r"$\frac{1}{4\cdot 9}\sum\left(\mathcal{M}^{\mathrm{V}}_{\mathrm{MRSSM}}(uu \to \tilde{u}_L\tilde{u}_R)\right)^2$")
+#plt.ylabel(r"$\frac{1}{4\cdot 9}\sum 2 \Re \left( \mathcal{M}^{\mathrm{B}} \mathcal{M}^{\mathrm{1L}\ast} \right)_{\mathrm{Virt}}(uu \to \tilde{u}_L\tilde{u}_R)$")
 #plt.axis([min(t/10**6) - 1.1,max(t/10**6) + 1.1,-0.05,0.25])
-#plt.plot(t/10**6, matrix_NLO_Virt, lw=2, ls="-", c='black', label = r"finite part of $\frac{1}{4\cdot 9}\sum(\mathcal{M}^{\mathrm{V}}_{\mathrm{MRSSM}})^2$")
-#plt.plot(t/10**6, matrix_NLO_Single, lw=2, ls="-", c='red', label = r"single pole of $\frac{1}{4\cdot 9}\sum(\mathcal{M}^{\mathrm{V}}_{\mathrm{MRSSM}})^2$")
-#plt.plot(t/10**6, matrix_NLO_Double, lw=2, ls="-", c='blue', label = r"double pole of $\frac{1}{4\cdot 9}\sum(\mathcal{M}^{\mathrm{V}}_{\mathrm{MRSSM}})^2$")
+#plt.plot(t/10**6, matrix_NLO_Virt, lw=2, ls="-", c='black', label = r"finite part of $\frac{1}{4\cdot 9}\sum 2 \Re \left( \mathcal{M}^{\mathrm{B}} \mathcal{M}^{\mathrm{1L}\ast} \right)_{\mathrm{Virt}}$")
+#plt.plot(t/10**6, matrix_NLO_Single, lw=2, ls="-", c='red', label = r"single pole of $\frac{1}{4\cdot 9}\sum 2 \Re \left( \mathcal{M}^{\mathrm{B}} \mathcal{M}^{\mathrm{1L}\ast} \right)_{\mathrm{Virt}}$")
+#plt.plot(t/10**6, matrix_NLO_Double, lw=2, ls="-", c='blue', label = r"double pole of $\frac{1}{4\cdot 9}\sum 2 \Re \left( \mathcal{M}^{\mathrm{B}} \mathcal{M}^{\mathrm{1L}\ast} \right)_{\mathrm{Virt}}$")
 
 # different contributions to |M|^2
-plt.ylabel(r"$\frac{1}{4\cdot 9}\sum(\mathcal{M}_{\mathrm{MRSSM}}(uu \to \tilde{u}_L\tilde{u}_R))^2$")
-plt.axis([min(t/10**6) - 1.1,max(t/10**6) + 1.1,-0.3,0.6])
-plt.plot(t/10**6, matrix_NLO_SE, lw=2, ls="--", c='green', label = r"$\frac{1}{4\cdot 9}\sum(\mathcal{M}^{\mathrm{Self-Energy}}_{\mathrm{MRSSM}})^2$")
-plt.plot(t/10**6, matrix_NLO_Vertex, lw=2, ls="--", c='red', label = r"$\frac{1}{4\cdot 9}\sum(\mathcal{M}^{\mathrm{Vertices}}_{\mathrm{MRSSM}})^2$")
-plt.plot(t/10**6, matrix_NLO_Boxes, lw=2, ls="--", c='blue', label = r"$\frac{1}{4\cdot 9}\sum(\mathcal{M}^{\mathrm{Boxes}}_{\mathrm{MRSSM}})^2$")
-plt.plot(t/10**6, matrix_NLO_Virt, lw=2, ls="-", c='black', label = r"$\frac{1}{4\cdot 9}\sum(\mathcal{M}^{\mathrm{V}}_{\mathrm{MRSSM}})^2$")
+#plt.ylabel(r"$\frac{1}{4\cdot 9}\sum 2\Re \left( \mathcal{M}^{\mathrm{B}} \mathcal{M}^{\mathrm{1L}\ast} \right)(uu \to \tilde{u}_L\tilde{u}_R)$")
+#plt.axis([min(t/10**6) - 1.1,max(t/10**6) + 1.1,-0.3,0.6])
+#plt.plot(t/10**6, matrix_NLO_SE, lw=2, ls="--", c='green', label = r"$\frac{1}{4\cdot 9}\sum 2 \Re \left( \mathcal{M}^{\mathrm{B}} \mathcal{M}^{\mathrm{1L}\ast} \right)_{\mathrm{Self-Energy}}$")
+#plt.plot(t/10**6, matrix_NLO_Vertex, lw=2, ls="--", c='red', label = r"$\frac{1}{4\cdot 9}\sum 2 \Re \left( \mathcal{M}^{\mathrm{B}} \mathcal{M}^{\mathrm{1L}\ast} \right)_{\mathrm{Vertices}}$")
+#plt.plot(t/10**6, matrix_NLO_Boxes, lw=2, ls="--", c='blue', label = r"$\frac{1}{4\cdot 9}\sum 2 \Re \left( \mathcal{M}^{\mathrm{B}} \mathcal{M}^{\mathrm{1L}\ast} \right)_{\mathrm{Boxes}}$")
+#plt.plot(t/10**6, matrix_NLO_Virt, lw=2, ls="-", c='black', label = r"$\frac{1}{4\cdot 9}\sum 2 \Re \left( \mathcal{M}^{\mathrm{B}} \mathcal{M}^{\mathrm{1L}\ast} \right)_{\mathrm{Virt}}$")
 #plt.plot(t/10**6, matrix_NLO_SE + matrix_NLO_Vertex + matrix_NLO_Boxes, lw=2, ls="--", c='green', label = "summed")
 
 plt.legend(loc=1,prop={'size':20})
