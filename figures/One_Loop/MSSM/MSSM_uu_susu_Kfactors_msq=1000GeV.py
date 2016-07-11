f7a8bbd9031f41e6a0715e6bcdab937fad8a8484
Sebastian Liebschner <sebastian-liebschner@gmx.de>
2016-07-11 10:05:39 +0200

    some changes

--- figures/One_Loop/MSSM/MSSM_uu_susu_Kfactors_msq=1000GeV.py
+++ figures/One_Loop/MSSM/MSSM_uu_susu_Kfactors_msq=1000GeV.py
@@ -59,13 +59,13 @@ K_min = 0
 K_max = 2
 
 fig = plt.figure(1,figsize=(12,8))
-font_size = 15
+font_size = 18
 #fig.suptitle(r'cross-sections for squark production at the LHC at $\sqrt{s}=13$TeV', fontsize=20)
 
 ############# first subplot #####################
 first_window = plt.subplot2grid((2,2), (0,0))
 plt.xlabel(r"$m_{\tilde{g}}$ in GeV", size = font_size)
-plt.ylabel(r"$K(uu \to \tilde{u}_L\tilde{u}_R)$ ", size = font_size)
+plt.ylabel(r"$K(pp \to \tilde{u}_L\tilde{u}_R)$ ", size = font_size)
 plt.xticks(fontsize = font_size)
 plt.yticks(fontsize = font_size)
 
@@ -87,7 +87,7 @@ plt.plot(m_gluino, sigma_NLO_1L_lr/sigma_LO_Tree_lr, lw=2, ls="-", c='blue')#, l
 ############# second subplot #####################
 second_window = plt.subplot2grid((2,2), (0,1))
 plt.xlabel(r"$m_{\tilde{g}}$ in GeV", size = font_size)
-plt.ylabel(r"$K(uu \to \tilde{u}_L\tilde{u}_L)$ ", size = font_size)
+plt.ylabel(r"$K(pp \to \tilde{u}_L\tilde{u}_L)$ ", size = font_size)
 plt.xticks(fontsize = font_size)
 plt.yticks(fontsize = font_size)
 
@@ -110,7 +110,7 @@ plt.plot(m_gluino, sigma_NLO_1L_ll/sigma_LO_Tree_ll, lw=2, ls="-", c='blue')#, l
 ############# third subplot #####################
 third_window = plt.subplot2grid((2,2), (1,0), colspan = 2)
 plt.xlabel(r"$m_{\tilde{g}}$ in GeV", size = font_size)
-plt.ylabel(r"$K(uu \to \tilde{u}\tilde{u})$ ", size = font_size)
+plt.ylabel(r"$K(pp \to \tilde{u}\tilde{u})$ ", size = font_size)
 plt.xticks(fontsize = font_size)
 plt.yticks(fontsize = font_size)
 
